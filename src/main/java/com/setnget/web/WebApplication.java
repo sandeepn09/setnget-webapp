c8a9ccb427084309031da218c784a72d4a01cf80
sandeepn26 <sandeepn26@gmail.com>
2020-10-18 01:07:32 -0400

    added war packaging

--- src/main/java/com/setnget/web/WebApplication.java
+++ src/main/java/com/setnget/web/WebApplication.java
@@ -2,8 +2,10 @@ package com.setnget.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.setnget.web")
 public class WebApplication {
 
 	public static void main(String[] args) {
